033fb58fa902fd15dd68e27ca278acf731134ec1
Ben <ben.pamittan@gmail.com>
2025-04-23 21:36:53 +1000

    productivity trackers for specific cost and specific timetable

--- src/utils/productivityTimer.ts
+++ src/utils/productivityTimer.ts
@@ -19,52 +19,40 @@ cron.schedule("* * * * *", async () => {
           }
         });
 
-        if (!actionCounters[tripId]) {actionCounters[tripId] = {};}
-            if (type == "AI" || type == "GUI") {
-                actionCounters[tripId][type] = 0;
-            }
-      }
-    }
-  }
-
-  for (const [tripId, typeCounts] of Object.entries(costActionCounters)) {
-    for (const [type, count] of Object.entries(typeCounts)) {
-      if (count > 0) {
-        // await db.action.create({
+        // await db.actionTimetable.create({
         //   data: {
         //     actionId: String(uuidv4()),
         //     tripId,
         //     type,
         //     dateTime: now.toISOString(),
-        //     count,
+        //     count: costActionCounters[tripId]?.[type] ?? 0,
         //   }
         // });
+
+        // await db.actionCost.create({
+        //   data: {
+        //     actionId: String(uuidv4()),
+        //     tripId,
+        //     type,
+        //     dateTime: now.toISOString(),
+        //     count: costActionCounters[tripId]?.[type] ?? 0,
+        //   }
+        // });
+
+        if (!actionCounters[tripId]) {actionCounters[tripId] = {};}
+        if (type == "AI" || type == "GUI") {
+            actionCounters[tripId][type] = 0;
+        }
 
         if (!costActionCounters[tripId]) {costActionCounters[tripId] = {};}
-            if (type == "AI" || type == "GUI") {
-                costActionCounters[tripId][type] = 0;
-            }
-      }
-    }
-  }
-
-  for (const [tripId, typeCounts] of Object.entries(timetableActionCounters)) {
-    for (const [type, count] of Object.entries(typeCounts)) {
-      if (count > 0) {
-        // await db.action.create({
-        //   data: {
-        //     actionId: String(uuidv4()),
-        //     tripId,
-        //     type,
-        //     dateTime: now.toISOString(),
-        //     count,
-        //   }
-        // });
+        if (type == "AI" || type == "GUI") {
+            costActionCounters[tripId][type] = 0;
+        }
 
         if (!timetableActionCounters[tripId]) {timetableActionCounters[tripId] = {};}
-            if (type == "AI" || type == "GUI") {
-                timetableActionCounters[tripId][type] = 0;
-            }
+        if (type == "AI" || type == "GUI") {
+            timetableActionCounters[tripId][type] = 0;
+        }
       }
     }
   }
